41409b8d02b06c838b2394aafdfcb09e0b81b2f8
fy <fythem@qq.com>
2022-12-23 12:04:26 +0800

    Update test.js

--- file/test.js
+++ file/test.js
@@ -5,7 +5,16 @@
 <script>
   window.Android_jsBridge.invoke('spotLeverTestSubmit', JSON.stringify({params: null}, -1), function (responseData) {
       document.write('responseData: ', responseData);
-    });
+  });
+  document.write("<br />" + "========")
+  document.write("<br />sensorsdata_call_app: " + window.SensorsData_APP_New_H5_Bridge.sensorsdata_call_app())
+  document.write("<br />sensorsdata_abtest_module: " + window.SensorsData_APP_New_H5_Bridge.sensorsdata_abtest_module())
+  document.write("<br />sensorsdata_get_app_visual_config: " + window.SensorsData_APP_New_H5_Bridge.sensorsdata_get_app_visual_config())
+  document.write("<br />sensorsdata_get_server_url：" + window.SensorsData_APP_New_H5_Bridge.sensorsdata_get_server_url())
+  document.write("<br />" + "========")
+  document.write("<br />sensorsdata_visualized_mode" + window.SensorsData_App_Visual_Bridge.sensorsdata_visualized_mode())
+  document.write("<br />")
+  document.write("<br />")
 </script>
 </body>
 </html>
